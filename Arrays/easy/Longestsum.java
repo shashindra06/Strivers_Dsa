cd4fcd5ab5b4acf5d7003b4daa79c376268c50ee
Shashindra Vadlakonda <shashindravadlakonda@gmail.com>
2026-08-31 18:04:50 +0530

    struggle

--- Arrays/easy/Longestsum.java
+++ Arrays/easy/Longestsum.java
@@ -1,5 +1,7 @@
 package Arrays.easy;
 
+//Given an array nums of size n and an integer k, find the length of the longest sub-array that sums to k. If no such sub-array exists, return 0.
+
 class Longestsum {
     public int longestSubarray(int[] nums, int k) {
         int n = nums.length;
---
+++ Arrays/easy/Medium/Twosum.java
@@ -0,0 +1,23 @@
+package Arrays.easy.Medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//Return indices of the two numbers such that their sum is equal to the target
+
+class Twosum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        // In case there is no solution, return an empty array
+        return new int[] {};
+    }
+}
