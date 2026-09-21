b3bb26fb88f4f03f914dc1e0a14f0e3e54f09fb1
Shashindra Vadlakonda <shashindravadlakonda@gmail.com>
2026-09-21 19:34:40 +0530

    recursion

---
+++ Recursion/Power.java
@@ -0,0 +1,28 @@
+package Recursion;
+
+class Power {
+    public double myPow(double x, int n) {
+
+        long N = n;
+
+        if (N < 0) {
+            N = -N;
+            return 1 / power(x, N);
+        }
+
+        return power(x, N);
+    }
+
+    public double power(double x, long n) {
+
+        if (n == 0)
+            return 1;
+
+        double half = power(x, n / 2);
+
+        if (n % 2 == 0)
+            return half * half;
+        else
+            return half * half * x;
+    }
+}
